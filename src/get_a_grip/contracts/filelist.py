b722fbaa69b0cc1a5df96c97f719d65d8e38dc57
Takashi SASAKI <takashi316@gmail.com>
2026-02-13 20:46:09 +0900

    feat: add gag FileItem vocabulary class and semantic annotations

--- src/get_a_grip/contracts/filelist.py
+++ src/get_a_grip/contracts/filelist.py
@@ -1,24 +1,24 @@
 """File listing data contracts."""
 
-from typing import List, TypedDict
+from typing import Annotated, List, TypedDict
 
 # Field mappings match https://purl.org/gag/schema/filelist.jsonld
 FileItem = TypedDict(
     "FileItem",
     {
-        "Filename": str,  # gag:fullPath
-        "Size": int,  # schema:contentSize
-        "Date Modified": str,  # gag:winFileTimeModified
-        "Date Created": str,  # gag:winFileTimeCreated
-        "Attributes": int,  # gag:fileAttributes
+        "Filename": Annotated[str, "https://purl.org/gag/schema/vocab#fullPath"],
+        "Size": Annotated[int, "https://schema.org/contentSize"],
+        "Date Modified": Annotated[str, "https://purl.org/gag/schema/vocab#winFileTimeModified"],
+        "Date Created": Annotated[str, "https://purl.org/gag/schema/vocab#winFileTimeCreated"],
+        "Attributes": Annotated[int, "https://purl.org/gag/schema/vocab#fileAttributes"],
     },
 )
 
 FileList = TypedDict(
     "FileList",
     {
-        "files": List[FileItem],  # gag:files
-        "dirs": List[FileItem],  # gag:directories
+        "files": Annotated[List[FileItem], "https://purl.org/gag/schema/vocab#files"],
+        "dirs": Annotated[List[FileItem], "https://purl.org/gag/schema/vocab#directories"],
     },
 )
 
